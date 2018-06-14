68e2061d45c3940e4f0ac2180f1c0ca23bf7f4c4
Francois Brodeur <francois.brodeur@adgear.com>
2018-06-14 11:46:19 -0400

    Add some tests

--- test/betree_tests.erl
+++ test/betree_tests.erl
@@ -8,6 +8,19 @@ basic_test() ->
     ok = erl_betree:betree_insert(Betree, 1, "i = 5"),
     {ok, Subs} = erl_betree:betree_search(Betree, "{\"i\": 5}"),
     ?assertEqual(Subs, [1]),
-    ok = erl_betree:betree_free(Betree),
-    ?assertEqual(true, true).
+    ok = erl_betree:betree_free(Betree).
+
+bad_insert_no_domain_test() ->
+    {ok, Betree} = erl_betree:betree_make(),
+    ?assertEqual(error, erl_betree:betree_insert(Betree, 1, "i = 5")),
+    ok = erl_betree:betree_free(Betree).
+
+bad_insert_bounded_string_test() ->
+    {ok, Betree} = erl_betree:betree_make(),
+    ok = erl_betree:betree_add_domain(Betree, "s|string|false|1"),
+    ok = erl_betree:betree_insert(Betree, 1, "s <> \"good\""),
+    ?assertEqual(error, erl_betree:betree_insert(Betree, 2, "s <> \"bad\"")),
+    {ok, Subs} = erl_betree:betree_search(Betree, "{\"s\": \"diff\"}"),
+    ?assertEqual(Subs, [1]),
+    ok = erl_betree:betree_free(Betree).
 
