21ffdee5540ea0a592e45a35577d6cb865717686
Francois Brodeur <francois.brodeur@adgear.com>
2018-10-23 15:04:00 -0400

    Test

--- lib_path.sh
+++
@@ -1,4 +0,0 @@
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/frankbro/be-tree/build"
-export BETREE_INCLUDE_DIR="/home/frankbro/be-tree/src"
-export BETREE_LIB_DIR="/home/frankbro/be-tree/build"
-
